d234a0d79bd298cc9def457dc4a6e47011c17168
Luis Alberto Ibarra Calderon <ibarra.luisalberto23@gmail.com>
2022-09-28 11:08:56 -0400

    Updated

---
+++ principal.cpp
@@ -0,0 +1,10 @@
+/*Nombre: Luis Alberto Ibarra Calderon
+C.I:7467171 Ch.
+C.U: 35-4620
+Correo: ibarra.luisalberto23@gmail.com*/
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "hola mundo";
+	return 0;
+}
